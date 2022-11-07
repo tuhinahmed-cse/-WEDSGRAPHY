f1cfffaf242e4797dd3a34c152856921cd48f802
tuhinahmed-cse <tuhinahmed.cse@gmail.com>
2022-11-07 23:24:08 +0600

    navbar add

--- src/Pages/SharedPages/Header/Header.js
+++ src/Pages/SharedPages/Header/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaCamera } from 'react-icons/fa';
 
 const Header = () => {
     return (
-        <div>
-        <h1>Hello</h1>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="info" variant="light">
+        <Container>
+          <Navbar.Brand ><FaCamera style={{fontSize:'36px'}}></FaCamera> <Link to='/' style={{textDecoration:'none', color: 'whitesmoke', fontWeight:'bold',}}> WEDSGRAPHY</Link></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link ><Link to='/' style={{textDecoration:'none', color: 'whitesmoke', fontWeight:'bold', marginLeft:'60px', fontSize:'16px'}}>HOME</Link></Nav.Link>
+              <Nav.Link ><Link to='/' style={{textDecoration:'none', color: 'whitesmoke', fontWeight:'bold', marginLeft:'60px', fontSize:'16px'}}>SERVICES</Link></Nav.Link>
+              <Nav.Link ><Link to='/' style={{textDecoration:'none', color: 'whitesmoke', fontWeight:'bold', marginLeft:'60px',fontSize:'18px'}}>BLOG</Link></Nav.Link>
+              
+            </Nav>
+            <Nav>
+              <Nav.Link ><Link to='/login'>Login</Link></Nav.Link>
+              
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
 };
 
--- src/Router/Router/Router.js
+++ src/Router/Router/Router.js
@@ -8,7 +8,14 @@ export const router = createBrowserRouter([
     path: '/',
     element: <Main></Main>,
     errorElement: <ErrorPage></ErrorPage>,
-    children: []
+    children: [
+
+        {
+            path: '/',
+            
+            
+        }
+    ]
 }
 
 
