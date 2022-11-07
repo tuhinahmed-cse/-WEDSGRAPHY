7947a24848de15e760e33885cf2a58d3c2107a18
tuhinahmed-cse <tuhinahmed.cse@gmail.com>
2022-11-07 22:31:54 +0600

    route create

---
+++ src/Router/Router/Router.js
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layouts/Main";
+import ErrorPage from "../../Pages/SharedPages/ErrorPage/ErrorPage";
+
+export const router = createBrowserRouter([
+
+{
+    path: '/',
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: []
+}
+
+
+])
